5bdf5f70657c4244cbbb0cfa8a08bcfddabcb9ee
Shevon Mendis <shevonmendis98@gmail.com>
2018-12-19 17:26:45 +0530

    change month on calendar

--- events.js
+++ events.js
@@ -1,3 +1,11 @@
+var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+var d = new Date();
+var currentMonth = d.getMonth();
+var currentYear = d.getFullYear();
+
+$("#month").text(months[currentMonth]);
+$("#year").text(currentYear);
+
 $("td").on("click", function(){
 	console.log($(this).text());
 });
@@ -7,11 +15,25 @@ $("h1").on("click", function(){
 });
 
 $(".fa-angle-left").on("click", function(event){
-	console.log("show last month");
+	if (currentMonth === 0){
+		currentMonth = 11;
+		currentYear--;
+	}else{
+		currentMonth--;
+	}
+	$("#month").text(months[currentMonth]);
+	$("#year").text(currentYear);
 	event.stopPropagation();
 });
 
 $(".fa-angle-right").on("click", function(event){
-	console.log("show next month");
+	if (currentMonth === 11){
+		currentMonth = 0;
+		currentYear++;
+	}else{
+		currentMonth++;
+	}
+	$("#month").text(months[currentMonth]);
+	$("#year").text(currentYear);
 	event.stopPropagation();
 });
