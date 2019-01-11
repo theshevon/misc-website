06a40b310b9e90f1c758c8336227ea8cb0a254a1
Shevon Mendis <shevonmendis98@gmail.com>
2019-01-11 13:54:20 +0530

    about page updates plus copyright

--- app.js
+++ app.js
@@ -23,7 +23,6 @@ var eventRoutes   = require("./routes/events"),
 /*==================================app config================================*/
 
 // connect to umisc database
-// mongodb://127.0.0.1:27017/umisc
 mongoose.connect("mongodb://umisc_admin:nimda_2019@ds253804.mlab.com:53804/umisc", {useNewUrlParser: true}, function(err, db) {
     if (err) {
         console.log('Unable to connect to the server. Please start the server. Error:', err);
@@ -73,11 +72,11 @@ app.use(adminRoutes);
 app.use(indexRoutes);
 
 //local
-// app.listen(3000, function(){
-//     console.log("Successfully connected to server");
-//   });
+app.listen(3000, function(){
+    console.log("Successfully connected to server");
+  });
 
 //production
-app.listen(process.env.PORT, process.env.IP, function(){
-  console.log("Successfully connected to server");
-});
+// app.listen(process.env.PORT, process.env.IP, function(){
+//   console.log("Successfully connected to server");
+// });
