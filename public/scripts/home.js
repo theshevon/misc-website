44f2425502cd88512066ca573fda0af4ae2861b2
Shevon Mendis <shevonmendis98@gmail.com>
2019-01-14 13:12:36 +0530

    working node mailer

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,3 +1,4 @@
+// fade in page
 window.onload = function(){
     $('#content, footer').fadeIn(1500);
 };
--- public/scripts/show-event.js
+++ public/scripts/show-event.js
@@ -1,18 +1,26 @@
+// if main delete button pressed
 $("#del-btn-1").on("click", function(event){
     $(".event-details").css("opacity", "0.1");
+
+    // hide all the present buttons
     $(".btn-set-1").each(function(i){
         $(this).css("visibility", "hidden");
     });
 
+    // show deletion confirmation overlay
     $(".confirm-overlay").css("opacity", "1");
 });
 
+// if cancel button pressed on deletion confirmation overlay
 $(".back-btn-2").on("click", function(event){
     $(".event-details").css("opacity", "1");
+
+    //  reveal the buttons hid by the overlay
     $(".btn-set-1").each(function(i){
         $(this).css("visibility", "visible");
     });
 
+    // hide the overlay
     $(".confirm-overlay").css("opacity", "0");
 })
 
